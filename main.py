a927ed1ef0844099c11fa0ad789754b22e1b02af
RiyaSuthar <47744649+RiyaSuthar@users.noreply.github.com>
2021-12-23 13:39:03 +0530

    tenth commit

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ import logging
 import datetime
 import functools
 import jwt
-import boto3
 
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
 
-ssm = boto3.client("ssm")
-JWT_SECRET = ssm.get_parameter(
-    Name='JWT_SECRET',
-    WithDecryption=True
-) 
+JWT_SECRET = os.environ.get('JWT_SECRET', 'myjwtsecret')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
